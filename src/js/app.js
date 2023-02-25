2647baf9d3c5dc766c0c0c292aa4f0141190dd8e
Alexander Schuster <alexander.schuster@web.de>
2023-02-25 10:32:24 +0100

    some more changes on top

--- src/js/app.js
+++ src/js/app.js
@@ -13,62 +13,77 @@ import '../scss/styles.scss';
     document.querySelector("#menu__home").addEventListener("click", () => {
         window.scrollTo(0, 0);
         document.querySelector("#menu").classList.remove("is-open");
+        document.querySelector("#header").classList.remove("header-positioned");
     });
     document.querySelectorAll("[name='jump-hautanalyse']").forEach(selector => selector.addEventListener("click", () => {
         scrollToElement("hautanalyse");
         document.querySelector("#menu").classList.remove("is-open");
+        document.querySelector("#header").classList.remove("header-positioned");
     }));
     document.querySelectorAll("[name='jump-gesicht']").forEach(selector => selector.addEventListener("click", () => {
         scrollToElement("gesicht");
         document.querySelector("#menu").classList.remove("is-open");
+        document.querySelector("#header").classList.remove("header-positioned");
     }));
     document.querySelectorAll("[name='jump-augen']").forEach(selector => selector.addEventListener("click", () => {
         scrollToElement("augen");
         document.querySelector("#menu").classList.remove("is-open");
+        document.querySelector("#header").classList.remove("header-positioned");
     }));
     document.querySelectorAll("[name='jump-haare-wachs']").forEach(selector => selector.addEventListener("click", () => {
         scrollToElement("haare_wachs");
         document.querySelector("#menu").classList.remove("is-open");
+        document.querySelector("#header").classList.remove("header-positioned");
     }));
     document.querySelectorAll("[name='jump-haare-diode']").forEach(selector => selector.addEventListener("click", () => {
         scrollToElement("haare_diode");
         document.querySelector("#menu").classList.remove("is-open");
+        document.querySelector("#header").classList.remove("header-positioned");
     }));
     document.querySelectorAll("[name='jump-microneedling']").forEach(selector => selector.addEventListener("click", () => {
         scrollToElement("microneedling");
         document.querySelector("#menu").classList.remove("is-open");
+        document.querySelector("#header").classList.remove("header-positioned");
     }));
     document.querySelectorAll("[name='jump-massagen']").forEach(selector => selector.addEventListener("click", () => {
         scrollToElement("massagen");
         document.querySelector("#menu").classList.remove("is-open");
+        document.querySelector("#header").classList.remove("header-positioned");
     }));
     document.querySelectorAll("[name='jump-manikuere']").forEach(selector => selector.addEventListener("click", () => {
         scrollToElement("manikuere");
         document.querySelector("#menu").classList.remove("is-open");
+        document.querySelector("#header").classList.remove("header-positioned");
     }));
     document.querySelectorAll("[name='jump-shellac']").forEach(selector => selector.addEventListener("click", () => {
         scrollToElement("shellac");
         document.querySelector("#menu").classList.remove("is-open");
+        document.querySelector("#header").classList.remove("header-positioned");
     }));
     document.querySelectorAll("[name='jump-fuss']").forEach(selector => selector.addEventListener("click", () => {
         scrollToElement("fuss");
         document.querySelector("#menu").classList.remove("is-open");
+        document.querySelector("#header").classList.remove("header-positioned");
     }));
     document.querySelectorAll("[name='jump-nagelpilz']").forEach(selector => selector.addEventListener("click", () => {
         scrollToElement("fuss_nagelpilz");
         document.querySelector("#menu").classList.remove("is-open");
+        document.querySelector("#header").classList.remove("header-positioned");
     }));
     document.querySelectorAll("[name='jump-eingewachsen']").forEach(selector => selector.addEventListener("click", () => {
         scrollToElement("fuss_eingewachsen");
         document.querySelector("#menu").classList.remove("is-open");
+        document.querySelector("#header").classList.remove("header-positioned");
     }));
     document.querySelectorAll("[name='jump-gutscheine']").forEach(selector => selector.addEventListener("click", () => {
         scrollToElement("gutscheine");
         document.querySelector("#menu").classList.remove("is-open");
+        document.querySelector("#header").classList.remove("header-positioned");
     }));
     document.querySelectorAll("[name='jump-kontakt']").forEach(selector => selector.addEventListener("click", () => {
         scrollToElement("kontakt");
         document.querySelector("#menu").classList.remove("is-open");
+        document.querySelector("#header").classList.remove("header-positioned");
     }));
 
     const scrollToElement = (elementName) => {
